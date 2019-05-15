b2091f6bca6efb9442f610a0fdae218c9cbe23fe
Yunus Emre Ertürk <45642601+YunusEmreErturk@users.noreply.github.com>
2019-05-16 02:35:05 +0300

    * Template tasarımında iyileştirmeler yapıldı. * Dinamik olarak ürünler listelendi. * Kategorilere göre ürünler listelendi. * Dinamik olarak ürün detay görüntülenmesi yapıldı.

--- E-rturkTeknoloji/Controllers/HomeController.cs
+++ E-rturkTeknoloji/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using E_rturkTeknoloji.Entity;
+using E_rturkTeknoloji.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,19 @@ namespace E_rturkTeknoloji.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var products = _context.Products.Where(x => x.IsApproved && x.IsHome).ToList();
+            var products = _context.Products
+                .Where(x => x.IsApproved && x.IsHome)
+                .Select(x => new ProductModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name.Length > 50 ? x.Name.Substring(0,47)+"...":x.Name,
+                    Description = x.Description.Length > 50 ? x.Description.Substring(0,47)+"...":x.Description,
+                    Price = x.Price,
+                    Image = x.Image == null ? "1.jpg" : x.Image,
+                    Stock = x.Stock,
+                    CategoryId = x.CategoryId
+                    
+                }).ToList();
             return View(products);
         }
         public ActionResult Details(int id)
@@ -22,11 +35,32 @@ namespace E_rturkTeknoloji.Controllers
 
             return View(productDetail);
         }
-        public ActionResult List()
+        public ActionResult List(int? id)
         {
-            var products = _context.Products.Where(x => x.IsApproved).ToList();
+            var products = _context.Products
+               .Where(x => x.IsApproved)
+               .Select(x => new ProductModel()
+               {
+                   Id = x.Id,
+                   Name = x.Name.Length > 50 ? x.Name.Substring(0, 47) + "..." : x.Name,
+                   Description = x.Description.Length > 50 ? x.Description.Substring(0, 47) + "..." : x.Description,
+                   Price = x.Price,
+                   Image = x.Image == null ? "1.jpg":x.Image,
+                   Stock = x.Stock,
+                   CategoryId = x.CategoryId
+
+               }).AsQueryable(); //sorgulamaya devam edebilirim.
+            if (id !=null)
+            {
+                products = products.Where(i => i.CategoryId == id);
+            }
             return View(products);
-            
+
+        }
+
+        public PartialViewResult GetCategories()
+        {
+            return PartialView(_context.Categories.ToList());
         }
         // deneme/2/2/2
     }
--- E-rturkTeknoloji/Models/ProductModel.cs
+++ E-rturkTeknoloji/Models/ProductModel.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace E_rturkTeknoloji.Entity
+namespace E_rturkTeknoloji.Models
 {
-    public class Product
+    public class ProductModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -13,9 +13,6 @@ namespace E_rturkTeknoloji.Entity
         public double Price { get; set; }
         public int Stock { get; set; }
         public string Image { get; set; }
-        public bool IsHome { get; set; }
-        public bool IsApproved { get; set; }
         public int CategoryId { get; set; }
-        public Category category { get; set; }
     }
 }
