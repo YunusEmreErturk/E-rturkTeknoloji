7dd3e5f7a06c00d2db5fc4125d2e334824b2289c
Yunus Emre Ertürk <yemrerturk@gmail.com>
2019-05-15 14:21:09 +0300

    Add project files.

---
+++ E-rturkTeknoloji/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using E_rturkTeknoloji.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_rturkTeknoloji.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext _context = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var products = _context.Products.Where(x => x.IsApproved && x.IsHome).ToList();
+            return View(products);
+        }
+        public ActionResult Details(int id)
+        {
+            var productDetail = _context.Products.Where(i => i.Id == id).FirstOrDefault();
+
+            return View(productDetail);
+        }
+        public ActionResult List()
+        {
+            var products = _context.Products.Where(x => x.IsApproved).ToList();
+            return View(products);
+            
+        }
+    }
+}
---
+++ E-rturkTeknoloji/Entity/DataInitializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_rturkTeknoloji.Entity
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var categories = new List<Category>()
+            {
+                new Category(){Name = "Camera",Description="Camera Products"},
+                new Category(){Name = "Computer",Description="Computer Products"},
+                new Category(){Name = "Phone",Description="Telefon Products"},
+                new Category(){Name = "Elektronic",Description="Electronic Products"}
+
+            };
+
+            foreach (var category in categories)
+            {
+                context.Categories.Add(category);
+            }
+            context.SaveChanges();
+
+            var products = new List<Product>()
+            {
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=false,CategoryId=1 },
+                new Product(){Name="Canon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=false,IsApproved=false,CategoryId=2 },
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=false,CategoryId=2 },
+                new Product(){Name="ACS 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=true,CategoryId=1 },
+                new Product(){Name="SGS 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=true,CategoryId=1 },
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=true,CategoryId=3 },
+                new Product(){Name="SDD 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=false,CategoryId=4 },
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=false,IsApproved=true ,CategoryId=4},
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell d",Price=1200,Stock=25,IsHome=true,IsApproved=false,CategoryId=1 },
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzel",Price=1200,Stock=25,IsHome=true,IsApproved=true,CategoryId=1 },
+                new Product(){Name="Nikon 4710",Description="DAHmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzell Hmet Megheerere beraere fgelirr guze l guzel",Price=1200,Stock=25,IsHome=true,IsApproved=true,CategoryId=1 },
+            };
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
