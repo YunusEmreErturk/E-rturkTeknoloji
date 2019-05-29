57e4764d0f1ebd545f8976fb9117d7a895d38012
yemrerturk@outlook.com <yemrerturk@outlook.com>
2019-05-29 14:45:55 +0300

    * Sepet Aktif edildi.  * Cart ve CartLine Sınıfı eklendi.

---
+++ E-rturkTeknoloji/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using E_rturkTeknoloji.Entity;
+using E_rturkTeknoloji.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_rturkTeknoloji.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+
+            if (cart==null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+
+            }
+            return cart;
+        }
+    }
+}
